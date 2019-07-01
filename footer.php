1e9b42299b7200269aaa88f2b305acae904ff01f
Adri Syahrul <adrydhelfa20@gmail.com>
2019-07-01 10:51:31 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-﻿	<section id="footer">
-		<p>Copyright © 2019 Database Designer APP Batam. All Rights Reserved.</p>
-	</section>
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/style.js"></script>
-		
-</body>
-</html>
